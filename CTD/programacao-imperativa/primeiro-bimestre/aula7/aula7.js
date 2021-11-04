4c5f173824dbfd0af1a38064f1d6929c2b1551a4
Arthur Campolina <arthurcampolina@hotmail.com>
2021-11-03 22:18:40 -0300

    atualização 03/11

---
+++ CTD/programacao-imperativa/primeiro-bimestre/aula7/aula7.js
@@ -0,0 +1,19 @@
+console.log (!true);
+console.log (!false);
+console.log (!!false);
+console.log (!!true);
+console.log (!1);
+console.log (!!1);
+console.log (!0);
+console.log (!!0);
+let x =5;
+let y = 9;
+x < 10 && x !==5;
+x > 9 || x === 5;
+(!x===y);
+
+let str = "hahaha";
+console.log (str);
+let eBonito = "false";
+!((str || msg) && eBonito);
+console.log (!!!"");
