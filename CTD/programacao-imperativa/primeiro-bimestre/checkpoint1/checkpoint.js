37c02d17a35d6ebc6042cb2ef479ad26ad039fbb
Arthur Campolina <arthurcampolina@hotmail.com>
2021-11-09 22:18:23 -0300

    Checkpoint 1

---
+++ CTD/programacao-imperativa/primeiro-bimestre/checkpoint1/checkpoint.js
@@ -0,0 +1,50 @@
+//Valor Padrão
+let pipoca ="pipoca";
+let tempoPipoca = 10;
+let macarrao ="macarrão";
+let tempoMacarra = 8;
+let carne ="carne";
+let tempoCarne = 15;
+let feijao = "feijão";
+let tempoFeijao = 12;
+let brigadeiro = "brigadeiro"
+let tempoBrigadeiro = 8;
+
+//Valor Inserido Pelo Usuário
+let comida = "macarrão";
+let tempo = 24;
+
+//Programa
+if (comida == pipoca && tempo > tempoPipoca && tempo <= 2*tempoPipoca){
+    console.log("A comida queimou!");
+} else if (comida == macarrao && tempo > tempoMacarra && tempo <= 2*tempoMacarra){
+    console.log("A comida queimou!");
+} else if (comida == carne && tempo > tempoCarne && tempo <= 2*tempoCarne){
+    console.log("A comida queimou!");
+} else if (comida == feijao && tempo > tempoFeijao && tempo <= 2*tempoFeijao){
+    console.log("A comida queimou!");
+} else if (comida == brigadeiro && tempo > tempoBrigadeiro && tempo <= 2*tempoBrigadeiro){
+    console.log("A comida queimou!");
+} else if (comida == pipoca && tempo < tempoPipoca){
+    console.log("Tempo insuficiente")
+} else if (comida == macarrao && tempo < tempoMacarra){
+    console.log("Tempo insuficiente")
+} else if (comida == carne && tempo < tempoCarne){
+    console.log("Tempo insuficiente")
+} else if (comida == feijao && tempo < tempoFeijao){
+    console.log("Tempo insuficiente")
+} else if (comida == brigadeiro && tempo < tempoBrigadeiro){
+    console.log("Tempo insuficiente")
+} else if (comida ==pipoca && tempo >= 3*tempoPipoca){
+    console.log("Kabumm!")
+} else if (comida ==macarrao && tempo >= 3*tempoMacarra){
+    console.log("Kabumm!")
+} else if (comida ==carne && tempo >= 3*tempoCarne){
+    console.log("Kabumm!")
+} else if (comida ==feijao && tempo >= 3*tempoFeijao){
+    console.log("Kabumm!")
+} else if (comida ==brigadeiro && tempo >= 3*tempoBrigadeiro){
+    console.log("Kabumm!")
+} else {
+    console.log("Prato pronto, bom apetite!!!")
+}
