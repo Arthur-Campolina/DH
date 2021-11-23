2e6e7ea4b4981730847832979f3fc34d7bc8746b
Arthur Campolina <arthurcampolina@hotmail.com>
2021-11-22 22:53:19 -0300

    atualização 22/11/21

--- CTD/programacao-imperativa/estudoAleatorio/teste.js
+++ CTD/programacao-imperativa/estudoAleatorio/teste.js
@@ -1,5 +1,21 @@
-function ePar(n){
-    let simnao = n%2 != 0 ? true:false;
-} 
+// function ePar(n){
+//     let simnao = n%2 != 0 ? true:false;
+// } 
 
-console.log(ePar(10));
+// console.log(ePar(10));
+
+// function longitudeNomeCompleto (nome, sobrenome){
+//   return nome.length + sobrenome.length + 1;
+// }
+// console.log(longitudeNomeCompleto("joao", "maria"));
+
+function maximo(n1, n2) {
+    let max = Math.abs(n2, n1);
+    if (max >= n1) {
+        return n1;
+    } else if (max >= n2) {
+        return n2;
+    }
+}
+
+console.log(maximo(15, 10));
