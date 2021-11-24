611e0348c25c2f0dcc40eccb229117f7afefaa83
Arthur Campolina <arthurcampolina@hotmail.com>
2021-11-23 22:37:35 -0300

    atualização 23/11/21

---
+++ CTD/programacao-imperativa/primeiro-bimestre/aula14/mesa.js
@@ -0,0 +1,10 @@
+// Criado pela Mesa
+const funcMultiplicacao = require ('./modulos/multiplicacao');
+
+console.log(funcMultiplicacao(5,5));
+
+
+//  Importado do site "npmjs.com"
+var validator = require("email-validator");
+ 
+console.log(validator.validate("batatinha123yahoo.com"));
