afe5a7c86a074029a30f52db80acc1cacc7d2216
Arthur Campolina <arthurcampolina@hotmal.com>
2022-03-07 20:47:04 -0300

    Aula5 Front End II

---
+++ CTD/Segundo Semestre/Front End II/Aula5/aula/script.js
@@ -0,0 +1,10 @@
+// Template String
+
+let user = "Paulo";
+
+function calc() {
+    return 4+4;
+}
+
+console.log(`Olá, ${user} ${calc()}`);
+
---
+++ CTD/Segundo Semestre/Front End II/Aula5/mesa/script.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+const item = document.querySelectorAll("item");
+const elemento = document.querySelectorAll("h2, p");
+
+function black() {
+  body.style.backgroundColor = "rgb(39, 39, 39)";
+
+  h1.style.backgroundColor = "rgb(39, 39, 39)";
+  h1.style.color = "rgb(170, 170, 170)";
+  h1.style.borderColor = "black";
+
+  item.backgroundColor = "rgb(19, 19, 19)";
+  item.borderColor = "black";
+
+  elemento.forEach(item => {
+      item.style.color = "red";
+      item.classList.toggle;
+  })
+}
+
+black();
