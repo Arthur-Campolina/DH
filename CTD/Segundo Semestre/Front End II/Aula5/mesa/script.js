2a30b90f553168abb7a19336cf4bf7bf0a778535
Arthur Campolina <arthurcampolina@hotmal.com>
2022-03-07 21:41:19 -0300

    Aula 5 front end II atualizado

--- CTD/Segundo Semestre/Front End II/Aula5/mesa/script.js
+++ CTD/Segundo Semestre/Front End II/Aula5/mesa/script.js
@@ -3,7 +3,7 @@ const h1 = document.querySelector("h1");
 const item = document.querySelectorAll("item");
 const elemento = document.querySelectorAll("h2, p");
 
-function black() {
+function darkMode() {
   body.style.backgroundColor = "rgb(39, 39, 39)";
 
   h1.style.backgroundColor = "rgb(39, 39, 39)";
@@ -15,8 +15,20 @@ function black() {
 
   elemento.forEach(item => {
       item.style.color = "red";
-      item.classList.toggle;
   })
 }
 
-black();
+function lightMode() {
+    body.style.backgroundColor = "white";
+  
+    h1.style.backgroundColor = "rgb(39, 39, 39)";
+    h1.style.color = "rgb(170, 170, 170)";
+    h1.style.borderColor = "black";
+  
+    item.backgroundColor = "rgb(19, 19, 19)";
+    item.borderColor = "black";
+  
+    elemento.forEach(item => {
+        item.style.color = "red";
+    })
+  }
