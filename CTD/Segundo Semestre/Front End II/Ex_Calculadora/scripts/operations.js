b9146719b3cf591dbbe8b49b6e645929bd484fb4
Arthur Campolina <arthurcampolina@hotmail.com>
2022-03-27 18:32:03 -0300

    Exercícios

---
+++ CTD/Segundo Semestre/Front End II/Ex_Calculadora/scripts/operations.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function subtraction(a ,b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+   return a * b;
+}
+function division(a, b) {
+    return a / b;
+}
