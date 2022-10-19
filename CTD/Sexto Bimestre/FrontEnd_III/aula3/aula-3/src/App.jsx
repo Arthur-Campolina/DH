a2b4c85ad4d8371ac87454cb4cc8a982254ecbdf
Arthur Campolina <arthurcampolina@hotmail.com>
2022-10-19 20:40:40 -0300

    FrontEnd III - ViteJS - Basics

---
+++ CTD/Sexto Bimestre/FrontEnd_III/aula3/aula-3/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button.jsx"
+import "./styles.css"
+
+function App() {
+
+  return (
+    <div className="mainDiv">
+      <h1>Olá</h1>
+      <Button />
+    </div>
+  )
+}
+
+export default App
