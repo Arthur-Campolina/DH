e285ce53aded48739acf6dcfa23261562df06312
Arthur Campolina <arthurcampolina@hotmail.com>
2023-06-18 15:54:03 -0300

    style: corrige nome das pastas

--- CTD/Sexto Bimestre/FrontEnd_III/aula18/mesa/src/pages/HotelDetails.jsx
+++ CTD/Sexto Bimestre/FrontEnd_III/aula18/mesa/src/pages/HotelDetails.jsx
@@ -1,7 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { hotels } from "../utils/hotels";
 
-
 export const HotelDetails = () => {
     const navigate = useNavigate();
     const { id } = useParams();
